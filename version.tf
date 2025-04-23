955d60be27ebeb2384ace57e8454abc3fc7646e2
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-04-23 15:19:12 -0400

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">=1.50.0"
+    }
+  }
+
+  cloud {
+    organization = "IBMGlobalCatalog"
+    workspaces {
+      name = "terraform-variables-sample"
+    }
+  }
+}
