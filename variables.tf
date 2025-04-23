6ae3ceeeab2b22035d1d8a73d28ca4f2bb930396
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-04-23 15:18:19 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "a" {
+  description = "A simple string type variable with a default value."
+  type        = string
+  default     = "our test"
+}
+variable "b" {
+  description = "A simple string type variable with a default value."
+  type        = string
+  default     = "B"
+}
+variable "c" {
+  description = "A simple string type variable with a default value."
+  type        = string
+  default     = "C"
+}
